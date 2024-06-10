a798edd47c56982a6ee8130407baf11669a51a3d
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-06-10 12:48:26 +0300

    test

--- src/pages/uploadPokemon/uploadPokemon.jsx
+++ src/pages/uploadPokemon/uploadPokemon.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import './UploadPokemon.css'
 
 const UploadPokemon = () => {
-  return <div>uploadPokemon</div>;
+  return <div>uploadPokemon
+  <h1>helo</h1></div>;
 };
 
 export default UploadPokemon;
